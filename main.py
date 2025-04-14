d44670cd66e3685e1a4caf42179ce917255041b2
capellodario <dariocapello97@gmail.com>
2025-04-14 15:40:46 +0200

    6

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ import os
 video_path = "/media/muchomas/rasp_key/1.mp4"  # Sostituisci con il percorso del tuo video lungo
 
 def play_spanned_video(video_path):
-    """Riproduce il video forzando la geometria sull'area spalmata."""
-    geometry = "7680x2160+0+0"  # Larghezza totale x Altezza + X offset + Y offset
+    """Riproduce il video adattando alla larghezza spalmata."""
     command = [
         "/usr/bin/mpv",
-        f"--geometry={geometry}",
+        f"--autofit=7680x2160",
+        "--video-align=0",
         "--loop",
         "--no-osc",
         video_path
     ]
-    print(f"Avvio video con geometria spalmata: {command}")
+    print(f"Avvio video adattando alla larghezza spalmata: {command}")
     process = subprocess.Popen(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     return process
 
 if __name__ == "__main__":
-    time.sleep(30)  # Attendi che l'ambiente desktop sia pronto
+    time.sleep(1)  # Attendi che l'ambiente desktop sia pronto
 
     video_process = play_spanned_video(video_path)
 
