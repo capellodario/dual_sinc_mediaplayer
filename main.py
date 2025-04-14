81f261598c1a01565ac73e5eaabcebb72d3f109d
capellodario <dariocapello97@gmail.com>
2025-04-14 16:08:37 +0200

    11

--- main.py
+++ main.py
@@ -2,31 +2,29 @@ import subprocess
 import time
 import os
 
-video_path = "/media/muchomas/rasp_key/1.mp4"  # Sostituisci con il nuovo percorso
+video_path = "/media/muchomas/rasp_key/1.mp4"
 
 def play_spanned_video(video_path):
-    """Riproduce il video a schermo intero."""
+    geometry = "7680x2160+0+0"
     command = [
         "/usr/bin/mpv",
         "--fullscreen=yes",
+        f"--geometry={geometry}",
         "--loop",
         "--no-osc",
         video_path
     ]
-    print(f"Avvio video a schermo intero: {command}")
+    print(f"Avvio video a schermo intero con geometria: {command}")
     process = subprocess.Popen(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     return process
 
 if __name__ == "__main__":
-    time.sleep(3)  # Attendi che l'ambiente desktop sia pronto
-
+    time.sleep(3)
     video_process = play_spanned_video(video_path)
-
     try:
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
-        print("Terminazione del processo video...")
         if video_process:
             video_process.terminate()
             video_process.wait()
