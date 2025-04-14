3da98014b42784549c66d9f275fc4e85520597c7
capellodario <dariocapello97@gmail.com>
2025-04-14 15:47:41 +0200

    9

--- main.py
+++ main.py
@@ -5,20 +5,23 @@ import os
 video_path = "/media/muchomas/rasp_key/1.mp4"  # Sostituisci con il percorso del tuo video lungo
 
 def play_spanned_video(video_path):
-    """Riproduce il video forzando a non mantenere le proporzioni."""
+    """Riproduce il video forzando fullscreen, geometria e ignorando le proporzioni."""
+    geometry = "7680x2160+0+0"
     command = [
         "/usr/bin/mpv",
+        "--fullscreen=yes",
+        f"--geometry={geometry}",
         "--no-keepaspect",
         "--loop",
         "--no-osc",
         video_path
     ]
-    print(f"Avvio video senza mantenere le proporzioni: {command}")
+    print(f"Avvio video forzando fullscreen, geometria e no-keepaspect: {command}")
     process = subprocess.Popen(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     return process
 
 if __name__ == "__main__":
-    time.sleep(3)  # Attendi che l'ambiente desktop sia pronto
+    time.sleep(30)  # Attendi che l'ambiente desktop sia pronto
 
     video_process = play_spanned_video(video_path)
 
