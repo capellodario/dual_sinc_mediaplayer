974ca34b5d6f917f98d907e9ad3397570c346315
capellodario <dariocapello97@gmail.com>
2025-04-15 15:39:08 +0200

    host_sistem_vlc-pyton

---
+++ test.py
@@ -0,0 +1,14 @@
+import vlc
+import time
+
+video_path = "/media/muchomas/rasp_key1/test_2.mp4"  # Assicurati che questo sia corretto
+
+instance = vlc.Instance()
+player = instance.media_player_new()
+media = instance.media_new(video_path)
+player.set_media(media)
+player.play()
+
+time.sleep(10)  # Riproduci per 10 secondi
+
+player.stop() 
