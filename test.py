f6c699dfb95099254cbb946a5644447dfe7190fc
capellodario <dariocapello97@gmail.com>
2025-04-15 15:40:37 +0200

    host_sistem_vlc-pyton

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import vlc
 import time
 
-video_path = "/media/muchomas/rasp_key1/test_2.mp4"  # Assicurati che questo sia corretto
+video_path = "/media/muchomas/rasp_key/test_2.mp4"  # Assicurati che questo sia corretto
 
 instance = vlc.Instance()
 player = instance.media_player_new()
